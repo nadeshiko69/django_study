257f78cd68ca857deaebbc28576b8678572a1753
Saku <ke.sakurauchi@gmail.com>
2020-12-11 00:25:44 +0900

    //9-2まで

--- venv_private_diary/bin/private_diary/diary/views.py
+++ venv_private_diary/bin/private_diary/diary/views.py
@@ -1,5 +1,9 @@
+import logging
+import django.urls import reverse_lazy
 from django.views import generic
 from .forms import InquiryForm
+
+logger = logging.getLogger(__name__)
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
@@ -7,3 +11,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry sent by{}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
